1c8049998df62844350a516970e6027335421b50
Jaap Jansma <jaap@edeveloper.nl>
2016-01-08 10:51:33 +0100

    rename land line to vaste telefoon

--- CRM/Tokens/Contact.php
+++ CRM/Tokens/Contact.php
@@ -34,14 +34,14 @@ class CRM_Tokens_Contact {
     }
 
     public static function tokens(&$tokens) {
-        $tokens['contact']['contact.landline'] = ts('Landline phone');
+        $tokens['contact']['contact.vaste_telefoon'] = ts('Vaste telefoon');
         $tokens['contact']['contact.mobile'] = ts('Mobile phone');
     }
 
     public function tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
         if (!empty($tokens['contact'])) {
-            if (in_array('landline', $tokens['contact']) || array_key_exists('landline', $tokens['contact'])) {
-                $this->landline($values, $cids, $job, $tokens, $context);
+            if (in_array('vaste_telefoon', $tokens['contact']) || array_key_exists('vaste_telefoon', $tokens['contact'])) {
+                $this->vaste_telefoon($values, $cids, $job, $tokens, $context);
             }
             if (in_array('mobile', $tokens['contact']) || array_key_exists('mobile', $tokens['contact'])) {
                 $this->mobile($values, $cids, $job, $tokens, $context);
@@ -65,16 +65,16 @@ class CRM_Tokens_Contact {
         }
     }
 
-    public function landline(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+    public function vaste_telefoon(&$values, $cids, $job = null, $tokens = array(), $context = null) {
         foreach($cids as $cid) {
-            $values[$cid]['contact.landline'] = '';
+            $values[$cid]['contact.vaste_telefoon'] = '';
             try {
                 $params = array();
                 $params['contact_id'] = $cid;
                 $params['phone_type_id'] = $this->landline_type;
                 $params['location_type_id'] = $this->home_location_type;
                 $params['return'] = 'phone';
-                $values[$cid]['contact.landline'] = civicrm_api3('Phone', 'getvalue', $params);
+                $values[$cid]['contact.vaste_telefoon'] = civicrm_api3('Phone', 'getvalue', $params);
             } catch (Exception $e) {
                 //do nothing
             }
